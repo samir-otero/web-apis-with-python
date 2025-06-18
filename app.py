a8f9b12344c5d560391d9f2315ed16e0e9fae9c8
ooterocarvajal <ooterocarvajal@rrc.ca>
2025-06-18 13:27:59 -0500

    Implemented I'm feeling lucky feature

--- app.py
+++ app.py
@@ -7,16 +7,22 @@ def index():
     """
     TODO: Render the home page provided under templates/index.html in the repository
     """
-    return "TODO"
+    return render_template( "index.html" )
 
 @app.get("/search")
 def search():
-	"""
-	TODO:
-	1. Capture the word that is being searched
-	2. Seach for the word on Google and display results
-	"""
-    return "TODO"
+    """
+    TODO:
+    1. Capture the word that is being searched
+    2. Seach for the word on Google and display results
+    """
+    args = request.args.get( "q" )
+    # if the button clicked is "Search", redirect to Google search results
+    if request.args.get("btn") == "Search":
+        return redirect(f"https://google.com/search?q={args}")
+    # if the button clicked is "I'm Feeling Lucky", redirect to the first result
+    elif request.args.get("btn") == "I'm Feeling Lucky":
+        return redirect(f"https://google.com/search?q={args}&btnI=1")
 
 if __name__ == "__main__":
     app.run()
